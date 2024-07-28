59b7e077430a4a965ee760e0824ea8f5c0171b6e
Eugene Alyoshin <aloyolreal@gmail.com>
2024-07-28 22:30:28 +0700

    проверка

--- hw_3/src/app.js
+++ hw_3/src/app.js
@@ -1,10 +1,17 @@
 const http = require("http");
 
+const fs = require("fs");
+
+const getUsers = () => {
+  return fs.readFileSync(path, './src/data/user.json')
+}
+
 const port = 3002;
 const hostname = "http://127.0.0.1";
 
 const server = http.createServer((request, response) => {});
 
-server.listen((port, hostname) => {
-  console.log(`Сервер запущен по адресу ${hostname}:${port}`);
-});
+server.listen(
+  port,
+  console.log(`Сервер запущен по адресу ${hostname}:${port}`)
+);
