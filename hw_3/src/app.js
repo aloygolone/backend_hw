ae1c9c067ed774391bbaa8f358c5a4c494561587
Eugene Alyoshin <aloyolreal@gmail.com>
2024-07-28 23:26:13 +0700

    Вообще непонятно, что от меня требуется

--- hw_3/src/app.js
+++ hw_3/src/app.js
@@ -1,15 +1,57 @@
 const http = require("http");
-
-const fs = require("fs");
-
-const getUsers = () => {
-  return fs.readFileSync(path, './src/data/user.json')
-}
+const getUsers = require("./modules/users");
+const getBooks = require("./modules/books");
 
 const port = 3002;
 const hostname = "http://127.0.0.1";
 
-const server = http.createServer((request, response) => {});
+const server = http.createServer((request, response) => {
+  if (request.url === "/users") {
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: application/json";
+    response.write(getUsers());
+    response.end();
+
+    return;
+  }
+
+  if (request.url === "/books") {
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: application/json";
+    response.write(getBooks());
+    response.end();
+
+    return;
+  }
+
+  if (request.url === "/") {
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: text/plain";
+    response.write("Enter request /books or /users");
+    response.end();
+
+    return;
+  }
+
+  if (request.url === "/?books/add-book") {
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: application/json";
+    response.write("Enter request /books or /users");
+    response.end();
+
+    return;
+  }
+
+  response.status = 500;
+  response.statusMessage = "Internal server error";
+  response.header = "Content-Type: text/plain";
+  response.write("");
+  response.end();
+});
 
 server.listen(
   port,
